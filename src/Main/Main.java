2ca806b3a62bef04d22f3fc7d874a36520e54911
Diegokwk1432 <diegocontreraskwk14@gmail.com>
2025-03-29 15:57:53 -0300

    Prueab2

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola");
 
-
+        for (int i=0; i<10; i++) {
+            System.out.println("Hola " + i);
+        }
 
 
 
